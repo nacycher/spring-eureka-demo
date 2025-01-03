1d371c208513a996e64d5c0d839a3f722b04d82e
nacycher <nacycher@163.com>
2025-01-03 13:40:47 +0800

    init project

---
+++ src/main/java/org/demo/springeurekademo/SpringEurekaDemoApplication.java
@@ -0,0 +1,13 @@
+package org.demo.springeurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaDemoApplication.class, args);
+    }
+
+}
